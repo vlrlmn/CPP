9c3988c04e0727a908469dafebb3d7b2892e16ab
vlrlmn <lomakina.valerie@gmail.com>
2024-08-09 21:45:42 +0400

    added getters

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,9 +6,28 @@
 /*   By: lomakinavaleria <lomakinavaleria@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 21:36:45 by lomakinaval       #+#    #+#             */
-/*   Updated: 2024/08/09 21:36:59 by lomakinaval      ###   ########.fr       */
+/*   Updated: 2024/08/09 21:45:26 by lomakinaval      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+int	Account::getNbAccounts( void )
+{
+    return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+    return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+    return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+    return (_totalNbWithdrawals);
+}
