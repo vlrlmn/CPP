24dc5511693d5a103aeeac16d88b408cec7f3faf
vlrlmn <122263940+vlrlmn@users.noreply.github.com>
2024-10-14 11:43:51 +0000

    added main functions

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -16,27 +16,29 @@ int main() {
     ClapTrap claptrap1("Clappy");
     ClapTrap claptrap2("Trappy");
 
-    std::cout << "\n--- Test Attack ---\n";
+    std::cout << "\n--- Test Attack ---" << std::endl;
     claptrap1.attack("Trappy");
     claptrap2.attack("Clappy");
 
-    std::cout << "\n--- Test Take Damage ---\n";
+    std::cout << "\n--- Test Take Damage ---" << std::endl;
     claptrap1.takeDamage(5);
     claptrap2.takeDamage(3);
 
-    std::cout << "\n--- Test Repair ---\n";
+    std::cout << "\n--- Test Repair ---" << std::endl;
     claptrap1.beRepaired(4);
     claptrap2.beRepaired(5);
 
-    std::cout << "\n--- Test Attack Without Energy ---\n";
+    std::cout << "\n--- Test Attack Without Energy ---" << std::endl;
     for (int i = 0; i < 10; ++i) {
         claptrap2.attack("Clappy");
     }
     claptrap2.attack("Clappy");
 
-    std::cout << "\n--- Test Repair Without Energy ---\n";
+    std::cout << "\n--- Test Repair Without Energy ---" << std::endl;
     claptrap2.beRepaired(3);
 
-    std::cout << "\n--- Test Copy Constructor and Assignment Operator ---\n";
+    std::cout << "\n--- Test Copy Constructor and Assignment Operator ---" << std::endl;
     ClapTrap claptrap3 = claptrap1;
+
+    std::cout << "\n--- End of Tests ---" << std::endl;
 }
