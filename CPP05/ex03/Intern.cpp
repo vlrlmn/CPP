9d7c5cdc817c58b306215f10fb6ccd755c63d7e4
vlrlmn <lomakina.valerie@gmail.com>
2025-03-14 18:51:44 +0400

    intern added constructor

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -1,11 +1,11 @@
 #include "Intern.hpp"
 
-Intern::Intern(): AForm() {}
+Intern::Intern() {}
 Intern::Intern(const std::string& name, int grade) {}
 Intern::Intern(const Bureaucrat &original) {}
 const Intern& Intern::operator=(const Bureaucrat &original){}
 Intern::~Intern() {}
 
-AForm &Intern::makeForm(std::string formName, std::string formTarget) {
+AForm *Intern::makeForm(std::string formName, std::string formTarget) {
     std::cout << "Intern creates " << formName << std::endl;
 }
