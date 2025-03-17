44ea1a7ee0e760e0a14547156c81e34f705a80cc
vlrlmn <lomakina.valerie@gmail.com>
2025-03-17 17:47:50 +0400

    finished ex02

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -4,67 +4,70 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-    AForm *shPtr = nullptr;
-    AForm *rPtr = nullptr;
-    AForm *pPtr = nullptr;
-    AForm *invalidPtr = nullptr;
-
-	Bureaucrat highRank("Alice", 2);
-	Bureaucrat lowRank("Bob", 149);
     try {
-        shPtr = new ShrubberyCreationForm("Garden");
-        rPtr = new RobotomyRequestForm("Robot");
-        pPtr = new PresidentialPardonForm("Criminal");
+        AForm *shPtr = nullptr;
+        AForm *rPtr = nullptr;
+        AForm *pPtr = nullptr;
+        AForm *invalidPtr = nullptr;
 
-        std::cout << std::endl << "-----------------FORMS CREATION-----------------" << std::endl;
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! During form creation: " << e.what() << std::endl;
+        Bureaucrat highRank("Alice", 2);
+        Bureaucrat lowRank("Bob", 149);
+        try {
+            shPtr = new ShrubberyCreationForm("Garden");
+            rPtr = new RobotomyRequestForm("Robot");
+            pPtr = new PresidentialPardonForm("Criminal");
+
+            std::cout << std::endl << "-----------------FORMS CREATION-----------------" << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! During form creation: " << e.what() << std::endl;
+        }
+
+        try {
+            invalidPtr = new PresidentialPardonForm("");
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! Failed to create an invalid form: " << e.what() << std::endl;
+        }
+
+        try {
+            std::cout << std::endl << "-----------------SIGNING FORMS-----------------" << std::endl;
+            highRank.signForm(*shPtr);
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! Signing Shrubbery: " << e.what() << std::endl;
+        }
+
+        try {
+            lowRank.signForm(*rPtr);
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! Signing Robotomy: " << e.what() << std::endl;
+        }
+
+        try {
+            std::cout << std::endl << "-----------------EXECUTING FORMS-----------------" << std::endl;
+            highRank.executeForm(*shPtr);
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! Executing Shrubbery: " << e.what() << std::endl;
+        }
+
+        try {
+            lowRank.executeForm(*rPtr);
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! Executing unsigned Robotomy: " << e.what() << std::endl;
+        }
+
+        try {
+            highRank.signForm(*pPtr);
+            highRank.executeForm(*pPtr);
+        } catch (const std::exception& e) {
+            std::cout << "EXCEPTION! Signing/executing Presidential: " << e.what() << std::endl;
+        }
+
+        std::cout << std::endl << "-----------------CLEANUP-----------------" << std::endl;
+        delete shPtr;
+        delete rPtr;
+        delete pPtr;
+        delete invalidPtr;
+    } catch (std::exception &e) {
+        std::cout << "EXCEPTION! " << e.what() << std::endl;
     }
-
-    try {
-        invalidPtr = new PresidentialPardonForm("");
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! Failed to create an invalid form: " << e.what() << std::endl;
-    }
-
-    try {
-        std::cout << std::endl << "-----------------SIGNING FORMS-----------------" << std::endl;
-        highRank.signForm(*shPtr);
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! Signing Shrubbery: " << e.what() << std::endl;
-    }
-
-    try {
-        lowRank.signForm(*rPtr);
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! Signing Robotomy: " << e.what() << std::endl;
-    }
-
-    try {
-        std::cout << std::endl << "-----------------EXECUTING FORMS-----------------" << std::endl;
-        highRank.executeForm(*shPtr);
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! Executing Shrubbery: " << e.what() << std::endl;
-    }
-
-    try {
-        lowRank.executeForm(*rPtr);
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! Executing unsigned Robotomy: " << e.what() << std::endl;
-    }
-
-    try {
-        highRank.signForm(*pPtr);
-        highRank.executeForm(*pPtr);
-    } catch (const std::exception& e) {
-        std::cout << "EXCEPTION! Signing/executing Presidential: " << e.what() << std::endl;
-    }
-
-    std::cout << std::endl << "-----------------CLEANUP-----------------" << std::endl;
-    delete shPtr;
-    delete rPtr;
-    delete pPtr;
-    delete invalidPtr;
-
     return 0;
 }
