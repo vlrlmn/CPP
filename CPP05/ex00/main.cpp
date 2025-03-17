25b66085b3c25af9d9cf44fa2805ecb287ec7918
vlrlmn <lomakina.valerie@gmail.com>
2025-03-17 17:52:11 +0400

    finished ex00

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -1,46 +1,37 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-    Bureaucrat bob("Bob", 10);
-    Bureaucrat alice("Alice", 1);
-    Bureaucrat trent("Trent", -1);
-    Bureaucrat mallory("Mallory", 151);
-
     try {
-        std::cout << "BOB" << std::endl;
-        bob.decrementGrade();
-        bob.decrementGrade();
-        std::cout << bob << std::endl;
-        bob.incrementGrade();
-        std::cout << bob << std::endl;
-    } catch (std::exception &e) {
-        std::cout << "EXCEPTION (Bob): " << e.what() << std::endl;
-    }
+        Bureaucrat bob("Bob", 10);
+        Bureaucrat alice("Alice", 1);
+        // Bureaucrat trent("Trent", -1);
+        // Bureaucrat mallory("Mallory", 151);
 
-    try {
-        std::cout << std::endl << "ALICE" << std::endl;
-        alice.decrementGrade();
-        std::cout << alice << std::endl;
-        alice.incrementGrade();
-        std::cout << alice << std::endl;
-        alice.incrementGrade();
-        std::cout << alice << std::endl;
-    } catch (std::exception &e) {
-        std::cout << "EXCEPTION (Alice): " << e.what() << std::endl;
-    }
+        try {
+            std::cout << "----------------BOB----------------" << std::endl;
+            bob.decrementGrade();
+            bob.decrementGrade();
+            std::cout << bob << std::endl;
+            bob.incrementGrade();
+            std::cout << bob << std::endl;
+        } catch (std::exception &e) {
+            std::cout << "EXCEPTION (Bob): " << e.what() << std::endl;
+        }
 
-    try {
-        std::cout << std::endl << "TRENT" << std::endl;
-        std::cout << trent << std::endl;
+        try {
+            std::cout << std::endl << "----------------ALICE----------------" << std::endl;
+            alice.decrementGrade();
+            std::cout << alice << std::endl;
+            alice.incrementGrade();
+            std::cout << alice << std::endl;
+            alice.incrementGrade();
+            std::cout << alice << std::endl;
+        } catch (std::exception &e) {
+            std::cout << "EXCEPTION (Alice): " << e.what() << std::endl;
+        }
+        std::cout << std::endl << "----------------CLEANUP----------------" << std::endl;
     } catch (std::exception &e) {
-        std::cout << "EXCEPTION (Trent): " << e.what() << std::endl;
-    }
-
-    try {
-        std::cout << std::endl << "MALLORY" << std::endl;
-        std::cout << mallory << std::endl;
-    } catch (std::exception &e) {
-        std::cout << "EXCEPTION (Mallory): " << e.what() << std::endl;
+        std::cout << "EXCEPTION! " << e.what() << std::endl;
     }
 
     return 0;
