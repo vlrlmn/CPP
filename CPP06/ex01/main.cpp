47426d3552834fb602213b1f9e707830fb28822f
vlrlmn <lomakina.valerie@gmail.com>
2025-03-20 17:39:45 +0400

    added ex01

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,25 +6,26 @@ int main() {
     original.age = 25;
     original.height = 1.68f;
 
+    std::cout << "---------DATA INIT----------" << std::endl;
     std::cout << "Original Data:" << std::endl;
     std::cout << "Name: " << original.name << std::endl;
     std::cout << "Age: " << original.age << std::endl;
     std::cout << "Height: " << original.height << std::endl;
     std::cout << "Original Address: " << &original << std::endl;
 
-    // Serialize
+    std::cout << std::endl << "---------SERIALIZING----------"<< std::endl;
     uintptr_t raw = Serializer::serialize(&original);
-    std::cout << "\nSerialized uintptr_t: " << raw << std::endl;
+    std::cout << "Serialized uintptr_t: " << raw << std::endl;
 
-    // Deserialize
+    std::cout << std::endl << "---------DESERIALIZING----------"<< std::endl;
     Data* deserialized = Serializer::deserialize(raw);
-    std::cout << "\nDeserialized Data:" << std::endl;
+    std::cout << "Deserialized Data:" << std::endl;
     std::cout << "Name: " << deserialized->name << std::endl;
     std::cout << "Age: " << deserialized->age << std::endl;
     std::cout << "Height: " << deserialized->height << std::endl;
     std::cout << "Deserialized Address: " << deserialized << std::endl;
 
-    //Adress coinsice
+    std::cout << std::endl << "---------CHECK OF COINSISING----------"<< std::endl;
     if (deserialized == &original) {
         std::cout << "Deserialization successful! Addresses match." << std::endl;
     } else {
