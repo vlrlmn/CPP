884b02d3ac25a400ec43e7da8eea740aef600af4
vlomakin <you@example.com>
2025-03-21 16:00:21 +0400

    added template

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: vlomakin <vlomakin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 15:51:20 by vlomakin          #+#    #+#             */
-/*   Updated: 2025/03/21 15:57:45 by vlomakin         ###   ########.fr       */
+/*   Updated: 2025/03/21 16:00:12 by vlomakin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
 template <typename T>
 T Max(const T& x, const T& y) {
     if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+template <typename T>
+T Min(const T& x, const T& y) {
+    if (x < y) {
         return x;
     } else {
         return y;
