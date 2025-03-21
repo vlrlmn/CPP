2fc904c84f765451f2027f986f1c23f5d4be27f6
vlomakin <you@example.com>
2025-03-21 15:57:59 +0400

    add template

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -6,12 +6,20 @@
 /*   By: vlomakin <vlomakin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 15:51:20 by vlomakin          #+#    #+#             */
-/*   Updated: 2025/03/21 15:51:21 by vlomakin         ###   ########.fr       */
+/*   Updated: 2025/03/21 15:57:45 by vlomakin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+template <typename T>
+T Max(const T& x, const T& y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
 
 #endif
