96ad9262239c216320acbc95ef3a8209b6d9d459
vlrlmn <lomakina.valerie@gmail.com>
2025-03-22 16:17:31 +0400

    added ex01

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,9 +1,22 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template<typename T>
-void iter(T* arr, T& len, T& func) {
+#include <iostream>
 
+template<typename T>
+void iter(T* const arr, const int len, void(*const f)(T const &)) {
+    for (int i = 0; i < len; i++)
+    {
+        f(arr[i]);
+    }
+}
+
+void printInt(int const &x) {
+    std::cout << x << std::endl;
+}
+
+void printString(std::string const &str) {
+    std::cout << str << std::endl;
 }
 
 #endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,5 +1,16 @@
-#include "iter.hpp"
+#include "Iter.hpp"
 
 int main() {
-
+    int numbers[] = {1, 2, 3, 4, 5};
+    std::string words[] = {"hello", "world", "template"};
+    std::string words2[] = {"", "world", "template"};
+    std::cout << std::endl << "----TEST1-----" << std::endl;
+    ::iter(numbers, 5, printInt);
+    std::cout << std::endl  << "----TEST2-----" << std::endl;
+    ::iter(words, 3, printString);
+    std::cout << std::endl << "----TEST3-----" << std::endl;
+    ::iter(words, 0, printString);
+    std::cout << std::endl << "----TEST4-----" << std::endl;
+    ::iter(words2, 3, printString);
+    return 0;
 }
