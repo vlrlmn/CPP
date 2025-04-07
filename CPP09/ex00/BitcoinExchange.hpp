230cb87f4c130eb83bba8123ddb043bb3e78ba81
vlrlmn <lomakina.valerie@gmail.com>
2025-04-07 18:23:52 +0400

    add

---
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,13 @@
+# pragma once
+
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <fstream>
+#include <string>
+#include <cctype> 
+
+#endif
---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,66 @@
+#include "BitcoinExchange.hpp"
+
+std::map<std::string, std::string> convertInput;
+
+std::string trim(const std::string& str) {
+    size_t start = 0;
+    while (start < str.length() && std::isspace(str[start]))
+        ++start;
+
+    size_t end = str.length();
+    while (end > start && std::isspace(str[end - 1]))
+        --end;
+
+    return str.substr(start, end - start);
+}
+
+bool validHeaderFormat(std::string line) {
+    return line == "date | value";
+}
+
+bool validFileContent(char *infile) {
+
+    std::ifstream in(infile);
+    if (!in.is_open())
+    {
+        std::cerr << "Error: could not open file." << std::endl;
+        return false;
+    }
+    else if (in.peek() == EOF)
+    {
+        std::cout << "File empty." << std::endl;
+        in.close();
+        return false;
+    }
+
+    std::string line;
+    if (std::getline(in, line)) {
+        if (!validHeaderFormat(line)) {
+            std::cerr << "Invalid header format" << std::endl;
+            in.close();
+            return false;
+        }
+    }
+    std::cout << "kek" << std::endl;
+    while(std::getline(in, line)) {
+
+    }
+
+    in.close();
+    return true;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        std::cerr << "Error: could not open file." << std::endl;
+        return -1;
+    }
+
+    char *infile = argv[1];
+    if (!validFileContent(infile)) {
+        return -1;
+    }
+    else
+        return 0;
+}
